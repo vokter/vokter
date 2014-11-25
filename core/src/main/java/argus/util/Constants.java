e04a165e812ad062fae1c54d676dded4aaa6a588
Ed Duarte <eduarte@ubiwhere.com>
2014-11-25 20:48:19 +0000

    Restructured project to separate plugins from core implementation.

---
+++ core/src/main/java/argus/util/Constants.java
@@ -0,0 +1,54 @@
+package argus.util;
+
+import argus.Main;
+
+import java.io.File;
+
+/**
+ * @author Eduardo Duarte (<a href="mailto:eduardo.miguel.duarte@gmail.com">eduardo.miguel.duarte@gmail.com</a>)
+ * @version 1.0
+ */
+public class Constants {
+
+    public static final String PROJECT_DIR = System.getProperty("user.dir")
+            + File.separator
+            + "src"
+            + File.separator
+            + "main";
+
+    public static final File INSTALL_DIR = new File(
+            Main.class.getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .getPath())
+            .getAbsoluteFile()
+            .getParentFile();
+
+    public static final File LANGUAGE_PROFILES_DIR =
+            new File(INSTALL_DIR, "lang-profiles");
+
+    public static final File READER_CLASSES_DIR =
+            new File(INSTALL_DIR, "readers" + File.separator + "argus");
+
+    public static final File STEMMER_CLASSES_DIR =
+            new File(INSTALL_DIR, "stemmers" + File.separator + "argus");
+
+    public static final File STOPWORDS_DIR =
+            new File(INSTALL_DIR, "stopwords");
+
+    public static final File DOCUMENTS_DIR =
+            new File(INSTALL_DIR, "documents");
+
+    public static final File TERMS_DIR =
+            new File(INSTALL_DIR, "mongodb");
+
+    public static long difference(long n1, long n2) {
+        long result = n1 - n2;
+        return result >= 0 ? result : -result;
+    }
+
+    public static int difference(int n1, int n2) {
+        int result = n1 - n2;
+        return result >= 0 ? result : -result;
+    }
+}
