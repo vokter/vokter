cfda3b9696b31ded994550551f574ba5cd33802f
Ed Duarte <eduarte@ubiwhere.com>
2014-12-12 15:09:38 +0000

    Fixed JSON reader module and added unit tests for all readers.

--- argus-readers/src/main/java/argus/reader/JsonReader.java
+++ argus-readers/src/main/java/argus/reader/JsonReader.java
@@ -2,13 +2,16 @@ package argus.reader;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import it.unimi.dsi.lang.MutableString;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Map;
 
 /**
  * A reader class that supports reading documents in the JSON format.
@@ -18,10 +21,14 @@ import java.util.Map;
  */
 public class JsonReader implements argus.reader.Reader {
 
+    private final Gson jsonParser;
+
+    public JsonReader() {
+        jsonParser = new Gson();
+    }
+
     @Override
     public MutableString readDocumentContents(InputStream documentStream) throws IOException {
-        Gson jsonParser = new Gson();
-
         MutableString sb = new MutableString();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(documentStream))) {
             for (int c; (c = reader.read()) != -1; ) {
@@ -29,16 +36,33 @@ public class JsonReader implements argus.reader.Reader {
             }
         }
 
-        Map data = jsonParser.fromJson(sb.toString(), Map.class);
+        JsonElement fullText = jsonParser.fromJson(sb.toString(), JsonElement.class);
         sb.delete(0, sb.length());
-        data.forEach((key, value) -> {
-            sb.append(key);
-            sb.append(' ');
-            sb.append(value);
-            sb.append(' ');
-        });
+        readRecursive(fullText, sb);
 
         return sb.compact();
+    }
+
+    private void readRecursive(JsonElement root, MutableString collector) {
+        if (root.isJsonObject()) {
+            JsonObject object = (JsonObject) root;
+            object.entrySet().forEach(e -> {
+                collector.append(e.getKey());
+                collector.append(' ');
+                readRecursive(e.getValue(), collector);
+                collector.append(' ');
+            });
+        } else if (root.isJsonArray()) {
+            JsonArray array = (JsonArray) root;
+            array.forEach(child -> {
+                readRecursive(child, collector);
+                collector.append(' ');
+            });
+        } else if (root.isJsonPrimitive()) {
+            JsonPrimitive primitive = (JsonPrimitive) root;
+            collector.append(primitive.getAsString());
+            collector.append(' ');
+        }
     }
 
     @Override
